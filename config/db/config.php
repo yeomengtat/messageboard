1222c6494d9187d2c780507b60992baf9107fd9a
yeomengtat <thelegacy95@hotmail.com>
2017-05-01 19:04:41 +0800

    modify config php

---
+++ config/db/config.php
@@ -0,0 +1,13 @@
+<?php
+/** 
+* db/config/config.php
+* XBF Database config file
+**/
+namespace Xodebox;
+Config::$dbConfig = [
+        'host' => 'localhost',
+        'user' => 'root',
+        'pass' => '123',
+        'name'   => 'message_test'
+];
+?>
