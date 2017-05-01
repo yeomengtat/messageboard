91c935e8c462aae349149411d993e80d7b3c746e
shath <shath.ibrahim@gmail.com>
2017-05-01 10:23:45 +0000

    Created user model

--- app/Models/User.class.php
+++ app/Models/User.class.php
@@ -4,7 +4,20 @@ use Xodebox\Model;
 class User extends Model{
     private $id, $name, $hash;
 
-    
+    public static function creator(){
+        return [
+            'id'    => 'pk',
+            'name'  => 'string',
+            'hash'  => 'string'
+        ];
+    }
+
+    /*
+    public function addRelations(){
+        return [
+            
+        ];
+        }*/
 }
 
 
